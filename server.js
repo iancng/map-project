4843ead531233fd860e0565328440fd5bdd400af
iancng <iancng2@gmail.com>
2023-02-07 20:30:50 +0900

    add socket

--- server.js
+++ server.js
@@ -2,16 +2,24 @@
 var express = require('express');
 //create our app
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 const PORT = process.env.PORT || 3030;
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    res.send('Hello Express!');
-    }
-);
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('location',(location)=>{
+        console.log(location);
+        socket.broadcast.emit('sharedLocation',location);
+    })
+});
 
-app.listen(PORT, function () {
-    console.log('Express server is up on port ' + PORT);
-    }
-);
+
+server.listen(PORT, () => {
+    console.log('Map server & socketIO listening on port' + PORT);
+});
